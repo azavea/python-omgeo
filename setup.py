27488012499d1c52429e93d955d4b22fbccdc566
Justin Walgran <jwalgran@azavea.com>
2012-03-15 10:05:32 -0400

    Add a 'test' command to setup.py and a script to run all UnitTest subclasses

--- setup.py
+++ setup.py
@@ -1,10 +1,27 @@
 #!/usr/bin/env python
 
 import os
-from setuptools import setup, find_packages
+import subprocess
+from setuptools import setup, find_packages, Command
 
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+class RunTests(Command):
+    description = "Run the unit test suite for python-omgeo."
+    user_options = []
+    extra_env = {}
+    extra_args = []
+
+    def run(self):
+        run_tests_script_path = os.path.join(os.path.dirname(__file__), 'omgeo', 'run_tests.py')
+        subprocess.call([run_tests_script_path])
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
 
 setup(
     name="python-omgeo",
@@ -15,6 +32,7 @@ setup(
     url="http://github.com/azavea/python-omgeo",
     license="MIT",
     long_description=read('README.rst'),
+    cmdclass = { 'test': RunTests },
     packages=find_packages(),
     classifiers=[
         'Development Status :: 4 - Beta',
