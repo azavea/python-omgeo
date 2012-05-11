cee1f70ec24344bc2e69103aaf76f74ad3ed370d
Joe <jtricarico@azavea.com>
2012-05-11 17:41:11 +0000

    update version no

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read(fname):
 
 setup(
     name="python-omgeo",
-    version="1.3.3",
+    version="1.3.4",
     description="A Python Geocoding Library",
     author="Azavea, Inc.",
     author_email="info@azavea.com",
