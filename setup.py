9de95ad9194b352b107a8411031e67f2a35be135
Andrew Jennings <ajennings@azavea.com>
2012-06-20 17:35:18 -0400

    Update version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read(fname):
 
 setup(
     name="python-omgeo",
-    version="1.4.0",
+    version="1.4.1",
     description="A Python Geocoding Library",
     author="Azavea, Inc.",
     author_email="info@azavea.com",
