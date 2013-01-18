ef3f78f647cf1de9141489b2b983115d44be28bd
Joseph Tricarico <jtricarico@azavea.com>
2013-01-18 13:43:27 -0500

    update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read(fname):
 
 setup(
     name="python-omgeo",
-    version="1.5.4",
+    version="1.5.5",
     description="Geocoding Library using ESRI, Bing Maps, and MapQuest geocoders",
     author="Azavea, Inc.",
     author_email="info@azavea.com",
