5e3862e856d401fa616244faadee659556959224
Michael Maurizi <mmaurizi@azavea.com>
2017-08-01 13:41:49 -0400

    Remove unsupported Esri NA & EU geocoder backends
    Fixes #37

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 2.6'
     ],
-    dependency_links=['http://github.com/htj/suds-htj/tarball/master#suds=0.4.1-htj'],
-    install_requires=['suds>=0.4'],
+    dependency_links=[],
+    install_requires=[],
     test_suite='omgeo.tests.tests',
 )
