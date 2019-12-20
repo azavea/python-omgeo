6f10e4cd4cb59cb91eed6869bb27213d11fe2f44
Klaas Hoekema <khoekema@azavea.com>
2019-12-19 23:48:48 -0500

    Update version specifiers to 6.0.4

--- sphinx/conf.py
+++ sphinx/conf.py
@@ -48,7 +48,7 @@ copyright = '2012-2018 Azavea'
 # built documents.
 #
 # The short X.Y version.
-version = '6.0.3'
+version = '6.0.4'
 # The full version, including alpha/beta/rc tags.
 release = version
 
